a465bf31c1d15a3b3a1977d820f106e6eb1a8049
Guillaume Gaillard <guillaumegaillard@users.noreply.github.com>
2020-12-22 17:51:48 +0100

    generalize standalonize

--- standalonize.py
+++ standalonize.py
@@ -25,16 +25,14 @@ def standalonize(begin, list_of_js, ext_css, list_of_css, remaining, dest='temp.
             destination.write(line)
     
 
-
-if __name__ == '__main__':
-
+def main_standal(html_file_str):
     begin_time=True
     remaining_time=False
 
     begin=[]
     remaining=[]
 
-    with open('Ukan_Izan.html', 'r') as main_html:
+    with open(html_file_str+".html", 'r') as main_html:
         for line in main_html:
             if "<script src=\"essentials.js\"></script>" in line:
                 begin_time=False
@@ -47,7 +45,7 @@ if __name__ == '__main__':
                 remaining.append(line)
 
     # list_of_js=["essentials.js","voca1.js","ukan_izan.js"]
-    list_of_js=["essentials.js","voca2.js","ukan_izan.js"]
+    list_of_js=["essentials.js","voca2.js",html_file_str.lower()+".js"]
 
     ext_css="""  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">\n"""
 
@@ -55,4 +53,12 @@ if __name__ == '__main__':
 
 
     # full_text=
-    standalonize(begin, list_of_js, ext_css, list_of_css, remaining, dest='Ukan_Izan_standalone.html')
+    standalonize(begin, list_of_js, ext_css, list_of_css, remaining, dest=html_file_str+'_standalone.html')
+
+
+if __name__ == '__main__':
+
+    main_standal('Ukan_Izan')
+    main_standal('Zenbakiak')
+
+    
